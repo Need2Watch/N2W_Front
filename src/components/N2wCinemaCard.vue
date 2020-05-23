1b91c12d2ba624fc62f26602ad0bdbba371bd6c6
AlexTheMagnus <i72lulem@uco.es>
2020-05-23 23:12:25 +0200

    Finished cinema card component

--- src/components/N2wCinemaCard.vue
+++ src/components/N2wCinemaCard.vue
@@ -1,26 +1,17 @@
 <template>
-  <v-card
-    class="card-cinema-card"
-    max-width="182"
-    max-height="269"
-  >
+  <v-card class="card-cinema-card" style="border-radius: 0;">
+      <v-img class="card-img" v-bind:src="this.image"/>
 
-      <v-img
-        class="card-img"
-        v-bind:src="this.image"
-      >
-      </v-img>
-
-      <v-card-title 
-        class="card-footer" 
-        max-width="182"
-      > 
-        <div class="card-footer-text">
+      <div class="card-footer">
+        <v-card-title class="card-footer-text">
           <p class="card-name">{{this.name}}</p>
+        </v-card-title>
+
+        <v-card-subtitle>
           <p class="card-rating-star">★ </p>
           <p class="card-rating-text">{{this.rating}}</p>
-        </div>
-      </v-card-title>
+        </v-card-subtitle>
+      </div>
   </v-card>
 </template>
 <script>
