84aac9942f851d7f4ecc43bba5badd891301b7bb
Francis Molina Prieto <francisjmp99@gmail.com>
2020-05-23 09:24:30 +0200

    Removed embedded CSS. Added img and styles folders.

--- src/components/N2wNavBar.vue
+++ src/components/N2wNavBar.vue
@@ -21,19 +21,16 @@
         <v-app-bar-nav-icon @click="sidebar = !sidebar"></v-app-bar-nav-icon>
       </span>
       <v-toolbar-items>
-        <div
-          class="hidden-md-and-down d-md-flex align-center"
-          style="max-width: 4%; margin-right: 10px"
-        >
+        <div class="hidden-md-and-down d-md-flex align-center n2w-logo-div">
           <a
             href="https://need2watch.com"
             aria-label="Need2Watch Home Page"
             title="Need2Watch Home Page"
-            style="width:100%"
+            class="a-logo"
           >
             <v-img
               aria-label="Need2WatchLogo"
-              src="../assets/logo.png"
+              src="../assets/img/logo.png"
               to="https://stackoverflow.com/questions/55779555/vuetify-create-image-that-links-to-another-page"
             ></v-img>
           </a>
@@ -66,13 +63,13 @@
             return-object
           ></v-autocomplete>
         </v-card-text>
-        <a href="/" style="text-decoration:none" class="d-md-flex align-center hidden-md-and-down">
+        <a href="/" class="decoration-none d-md-flex align-center hidden-md-and-down">
           <v-icon transparent size="40">mdi-calendar</v-icon>
         </a>
         <v-card flat class="transparent d-md-flex align-center hidden-md-and-down">
           <v-list-item href="/">
-            <v-avatar size="50" style="margin-right:10px">
-              <v-img src="../assets/avatar.jpg"></v-img>
+            <v-avatar size="50" class="nav-bar-avatar">
+              <v-img src="../assets/img/avatar.jpg"></v-img>
             </v-avatar>
             <v-list-item-title class="headline">Francis Molina</v-list-item-title>
           </v-list-item>
@@ -85,7 +82,7 @@
 
             <v-list>
               <v-list-item v-for="(item, i) in items" :key="i" @click="_">
-                <a :href="item.path" style="text-decoration: none; color: white">
+                <a :href="item.path" class="decoration-none a-white">
                   <v-list-item-title>{{ item.title }}</v-list-item-title>
                 </a>
               </v-list-item>
@@ -117,3 +114,6 @@ export default {
   }
 };
 </script>
+<style scoped>
+@import "../assets/styles/N2wNavBar.css";
+</style>
