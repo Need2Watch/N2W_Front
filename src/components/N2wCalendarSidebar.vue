a561b5e38b4ba9710d8543520d6649aa77063d7a
Francis Molina Prieto <francisjmp99@gmail.com>
2020-05-24 22:38:33 +0200

    Everything looks better now

--- src/components/N2wCalendarSidebar.vue
+++ src/components/N2wCalendarSidebar.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="calendarSideBar">
-    <div class="d-flex flex-column align-center">
-      <div class="profileBanner">
-        <div class="profileCircle"></div>
-      </div>
-      <div class="sidebarInfo d-flex flex-column align-center">
+  <div class="calendarSideBar d-flex flex-column align-center">
+    <div class="profileBanner d-flex flex-column align-center">
+      <div class="profileCircle"></div>
+    </div>
+    <div class="sidebarInfo d-flex flex-column align-center">
+      <router-link
+        to="/profile"
+        class="decoration-none hidden-md-and-down d-flex flex-column align-center"
+      >
         <a class="profileName">Francis Molina</a>
         <a class="profileUsername">rexuswolf</a>
-        <v-list id="event-list" class="eventList overflow-y-auto" color="transparent">
-          <v-list-item-group v-model="item">
-            <v-list-item v-for="(item, i) in events" :key="i">
-              <v-list-item-content>
-                <v-list-item-title v-text="item.text"></v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </div>
+      </router-link>
+      <v-list
+        max-height="500px"
+        id="event-list"
+        class="eventList overflow-y-auto"
+        color="transparent"
+      >
+        <v-list-item-group v-model="item">
+          <v-list-item v-for="(item, i) in events" :key="i">
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </div>
   </div>
 </template>
@@ -31,9 +39,13 @@ export default {
       { text: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
       { text: 'Mon, Wed, Fri', value: [1, 3, 5] },
       { text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
+      { text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
       { text: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
       { text: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
-      { text: 'Mon, Wed, Fri', value: [1, 3, 5] },
+      { text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
+      { text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
+      { text: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
+      { text: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
     ],
   }),
 };
