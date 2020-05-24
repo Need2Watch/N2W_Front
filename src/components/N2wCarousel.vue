c2db03ede27e0fca6741d8033707f931e3ad7cc5
AlexTheMagnus <i72lulem@uco.es>
2020-05-24 18:19:40 +0200

    Fixed carousel (still embebed cards)

--- src/components/N2wCarousel.vue
+++ src/components/N2wCarousel.vue
@@ -1,79 +1,93 @@
 <template>
-    <div class="card-carousel-wrapper">
-        <div class="card-carousel--nav__left" @click="moveCarousel(-1)" :disabled="atHeadOfList"></div>
-        <div class="card-carousel">
-        <div class="card-carousel--overflow-container">
-            <div class="card-carousel-cards" :style="{ transform: 'translateX' + '(' + currentOffset + 'px' + ')' }">
-            <div class="card-carousel--card" :key="item.title" v-for="item in items">
-                <div class="background-card" @mouseover="mouseOver(item.name)" @mouseleave="mouseLeave(item.name)"><img v-bind:src="item.image"/>
-                <div class="card-footer" v-show="item.overlay">
-                    <p>{{ item.name }}</p>
-                    <p--rating--star>★</p--rating--star>
-                    <p--rating>{{ item.rating }}</p--rating>
-                </div>
-                </div>
+  <div class="card-carousel-wrapper">
+    <div class="card-carousel--nav__left" @click="moveCarousel(-1)" :disabled="atHeadOfList"></div>
+    <div class="card-carousel">
+      <div class="card-carousel--overflow-container">
+        <div
+          class="card-carousel-cards"
+          :style="{ transform: 'translateX' + '(' + currentOffset + 'px' + ')' }"
+        >
+          <div class="card-carousel--card" :key="item.title" v-for="item in items">
+            <div
+              class="background-card"
+              @mouseover="mouseOver(item.name)"
+              @mouseleave="mouseLeave(item.name)"
+            >
+              <img v-bind:src="item.image" />
+              <div class="card-footer" v-show="item.overlay">
+                <p>{{ item.name }}</p>
+                <p--rating--star>★</p--rating--star>
+                <p--rating>{{ item.rating }}</p--rating>
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-        </div>
-        <div class="card-carousel--nav__right" @click="moveCarousel(1)" :disabled="atEndOfList"></div>
+      </div>
     </div>
-
+    <div class="card-carousel--nav__right" @click="moveCarousel(1)" :disabled="atEndOfList"></div>
+  </div>
 </template>
 <script>
 export default {
-  name: "N2wCarousel",
-  template: "#v-carousel",
+  created() {
+    this.windowResize();
+    window.addEventListener('resize', this.resizeEventHandler);
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.resizeEventHandler);
+  },
+  name: 'N2wCarousel',
+  template: '#v-carousel',
   data() {
     return {
       currentOffset: 0,
       windowSize: 5,
       paginationFactor: 262,
-    
-    items: [
+
+      items: [
         {
-        name: "Doctor Who",
-        rating: 10,
-        image: "https://placehold.it/182x269",
-        overlay: false
+          name: 'Doctor Who',
+          rating: 10,
+          image: 'https://placehold.it/182x269',
+          overlay: false,
         },
         {
-        name: "The Mandalorian: The las jedi episode",
-        rating: 9.8,
-        image: "https://placehold.it/182x269",
-        overlay: false
+          name: 'The Mandalorian: The las jedi episode',
+          rating: 9.8,
+          image: 'https://placehold.it/182x269',
+          overlay: false,
         },
         {
-        name: "1917",
-        rating: 5.4,
-        image: "https://placehold.it/182x269",
-        overlay: false
+          name: '1917',
+          rating: 5.4,
+          image: 'https://placehold.it/182x269',
+          overlay: false,
         },
         {
-        name: "Fight Club",
-        rating: 6.2,
-        image: "https://placehold.it/182x269",
-        overlay: false
+          name: 'Fight Club',
+          rating: 6.2,
+          image: 'https://placehold.it/182x269',
+          overlay: false,
         },
         {
-        name: "Narcos",
-        rating: 7,
-        image: "https://placehold.it/182x269",
-        overlay: false
+          name: 'Narcos',
+          rating: 7,
+          image: 'https://placehold.it/182x269',
+          overlay: false,
         },
         {
-        name: "The Office",
-        rating: 3.5,
-        image: "https://placehold.it/182x269",
-        overlay: false
+          name: 'The Office',
+          rating: 3.5,
+          image: 'https://placehold.it/182x269',
+          overlay: false,
         },
         {
-        name: "You",
-        rating: 8,
-        image: "https://placehold.it/182x269",
-        overlay: false
-        }
-    ]
+          name: 'You',
+          rating: 8,
+          image: 'https://placehold.it/182x269',
+          overlay: false,
+        },
+      ],
     };
   },
   computed: {
@@ -85,7 +99,7 @@ export default {
     },
     atHeadOfList() {
       return this.currentOffset === 0;
-    }
+    },
   },
   methods: {
     moveCarousel(direction) {
@@ -110,44 +124,37 @@ export default {
         }
       }
     },
+    resizeEventHandler() {
+      this.windowResize();
+    },
     windowResize() {
       var preSize = this.windowSize;
-      
-      if (window.matchMedia("(max-width: 554px)").matches) {
-        console.log(1)
-        this.windowSize = 1;
-        console.log("la ventana es " + this.windowSize)
-        return 0;
-      }
-      if (window.matchMedia("(max-width: 816px)").matches) {
-        console.log(2)
-        this.windowSize = 2;
-        console.log("la ventana es " + this.windowSize)
-        return 0;
-      }
-      if (window.matchMedia("(max-width: 1078px)").matches) {
-        console.log(3)
-        this.windowSize = 3;
-        console.log("la ventana es " + this.windowSize)
-        return 0;
-      }
-      if (window.matchMedia("(max-width: 1340px)").matches) {
-        console.log(4)
-        this.windowSize = 4;
-        console.log("la ventana es " + this.windowSize)
-        return 0;
-      }
-      console.log(5)
-      this.windowSize = 5;
-      console.log("la ventana es " + this.windowSize)
 
-      if(preSize < this.windowSize){
-        for(var i = 0; i < (this.windowSize - preSize); i++) {
-          this.moveCarousel(-1)
+      if (window.matchMedia('(max-width: 554px)').matches) {
+        this.windowSize = 1;
+        return 0;
+      }
+      if (window.matchMedia('(max-width: 816px)').matches) {
+        this.windowSize = 2;
+        return 0;
+      }
+      if (window.matchMedia('(max-width: 1078px)').matches) {
+        this.windowSize = 3;
+        return 0;
+      }
+      if (window.matchMedia('(max-width: 1340px)').matches) {
+        this.windowSize = 4;
+        return 0;
+      }
+      this.windowSize = 5;
+
+      if (preSize < this.windowSize) {
+        for (var i = 0; i < this.windowSize - preSize; i++) {
+          this.moveCarousel(-1);
         }
       }
-    }
-  }
+    },
+  },
 };
 </script>
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,20 @@
 <template>
   <div>
-    <n2w-cinema-card name="Doctor Who" rating=9.8 image="https://placehold.it/182x269"></n2w-cinema-card>
+    <n2w-carousel></n2w-carousel>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import N2wCinemaCard from '../components/N2wCinemaCard.vue'
+import N2wCarousel from '../components/N2wCarousel.vue';
 export default {
   name: 'Home',
   components: {
-    N2wCinemaCard,
+    N2wCarousel,
   },
   data() {
-    return {
-    }
+    return {};
   },
 };
 </script>
