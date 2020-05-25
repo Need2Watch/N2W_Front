b57c9cd4ff0a0b08d3421fa644ac2939ff09ab56
AlexTheMagnus <i72lulem@uco.es>
2020-05-26 00:29:23 +0200

    Added contactUsForm component and modified contactUs view

---
+++ src/components/N2wContactUsForm.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="contactUsForm">
+        <p class="tittle"> GET IN TOUCH </p>
+
+        <form class="" @submit.prevent="onSubmit">
+            <p>
+                <input class="textInputs" type="text" id="name" v-model="name" placeholder="Name..." required>
+            </p>
+            <p>
+                <input class="textInputs" type=text id="email" v-model="email" placeholder="Email..." required>
+            </p>
+            <p>
+                <input class="textInputs" type=text id="regarding" v-model="regarding" placeholder="Regarding..." required>
+            </p>
+            <p>
+                <textarea class="textInputs" type=text id="message" v-model="message" placeholder="Message...(10 character minimum)" required></textarea>
+            </p>
+            <p class="terms-text">
+                <input type="checkbox" id="terms" v-model="terms" required>
+                I have read and accept the privacy policy.
+            </p>
+            <p>
+                <input type="submit" value="SEND IT!">  
+            </p>    
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name: 'N2wContactUsForm',
+    data() {
+        return {
+            name: "",
+            email: "",
+            regarding: "",
+            message: "",
+            terms: false,
+        };
+    },
+    methods:{
+        onSubmit(){
+            let contactMessage = {
+                name: this.name,
+                email: this.email,
+                regarding: this.regarding,
+                message: this.message
+            }
+            console.log(contactMessage)                                //Enviar el contactMessage
+            this.$emit("contact-us-message-submitted", contactMessage) //Llama a la pag de success
+        }
+    }
+};
+</script>
+<style scoped>
+@import '../assets/styles/N2wContactUsForm.css';
+</style>
--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
-    <h1>This is an contact us page</h1>
+  <div class="contactUsMainDiv">
+
+    <div class="formDiv" style="width: 40%; float:left;">
+      <N2wContactUsForm></N2wContactusForm>
+    </div>
+    
+    <div class="socialDiv" style="float:left; margin-top: 100px;">
+      <v-list-item style="margin:50px;">
+        <v-avatar size="100">
+          <v-img src="../assets/img/facebook.png"></v-img>
+        </v-avatar>
+
+        <v-list-item-title  style="margin-left:10px; font-family: 'Bungee', cursive; font-size: xx-large;">NEED2WATCH</v-list-item-title>
+      </v-list-item>
+      
+      <v-list-item style="margin:50px;">
+        <v-avatar size="100">
+          <v-img src="../assets/img/twitter.png"></v-img>
+        </v-avatar>
+
+        <v-list-item-title style="margin-left:10px; font-family: 'Bungee', cursive; font-size: xx-large;">@NEED2WATCH</v-list-item-title>
+      </v-list-item>
+    </div>
+
   </div>
 </template>
+
+<script>
+import N2wContactUsForm from '../components/N2wContactUsForm.vue';
+export default {
+  name: 'ContactUs',
+  components: {
+    N2wContactUsForm,
+  },
+};
+</script>
