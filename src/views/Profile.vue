493cb5f80bf378a1c7474b81e6fcaba39864fa20
Francis Molina Prieto <francisjmp99@gmail.com>
2020-05-25 17:29:08 +0200

    URL is from gravatar now

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,18 +1,18 @@
 <template>
-  <v-card color="transparent" class="d-flex movieCard" height="100%" width="100%">
-    <v-card class="moviePoster" style="overflow:hidden" height="90%" width="40%">
-      <v-img :src="moviePosterUrl"></v-img>
-    </v-card>
+  <v-card color="transparent" class="d-flex profileCard" height="100%" width="100%">
+    <div class="d-flex flex-column align-center">
+      <v-avatar size="400" class="nav-bar-avatar">
+        <v-img :src="userAvatarURL"></v-img>
+      </v-avatar>
+      <h1>{{userFirstName}} {{userLastName}}</h1>
+      <h2>{{userName}}</h2>
+      <h2>{{userEmail}}</h2>
+      <h2>{{userCity}} {{userCountry}}</h2>
+    </div>
     <div style="height: 100%; width: 70%" class="d-flex flex-column">
-      <div class="movieMainInfo d-flex">
-        <v-card-title class="movieTitle">{{movieTitle}}</v-card-title>
-        <div class="d-flex flex-column">
-          <h2>Rating: {{movieRating}}</h2>
-          <h2>Genre: {{movieGenre}}</h2>
-        </div>
-      </div>
+      <v-card-title class="userBioHeader">Biography</v-card-title>
       <v-card-text>
-        <p class="movieOverview">{{movieOverview}}</p>
+        <p class="userBio">{{userBio}}</p>
       </v-card-text>
     </div>
   </v-card>
@@ -22,27 +22,32 @@ export default {
   name: 'Profile',
   props: {
     /*
-    movieTitle: String,
-    moviePosterUrl_: String,
-    movieRating_: String,
-    movieGenre_: String,
-    movieOverview_: String,
+      userFirstName: String,
+      userLastName: String,
+      userName: String,
+      userEmail: String,
+      userCountry: String,
+      userCity: String,
+      userBio: String,
+      userAvatarURL: String,
     */
   },
   components: {},
   data() {
     return {
-      movieTitle: '1917',
-      moviePosterUrl:
-        'https://i1.wp.com/noescinetodoloquereluce.com/wp-content/uploads/2019/08/1917-poster.jpg',
-      movieRating: '9,6',
-      movieGenre: 'War, Drama',
-      movieOverview:
-        'April 6th, 1917. As a regiment assembles to wage war deep in enemy territory, two soldiers are assigned to race against time and deliver a message that will stop 1,600 men from walking straight into a deadly trap.',
+      userFirstName: 'Francis',
+      userLastName: 'Molina',
+      userName: 'rexuswolf',
+      userEmail: 'francisjmp99@gmail.com',
+      userCountry: 'España',
+      userCity: 'Córdoba',
+      userBio: 'This is the user biography',
+      userAvatarURL:
+        'https://es.gravatar.com/userimage/148655767/f61f068eaac5470225f8923c99de0778.jpg?size=400',
     };
   },
 };
 </script>
 <style scoped>
-@import '../assets/styles/Movie.css';
+@import '../assets/styles/Profile.css';
 </style>
