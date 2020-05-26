3b367cb8cd15e9fb5a3a50093ff703a07f99ca49
AlexTheMagnus <i72lulem@uco.es>
2020-05-26 17:33:38 +0200

    Added contactUsSuccess page

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,14 @@ const routes = [
     component: Home
   },
   {
+    path: '/contactUsSuccess',
+    name: 'ContactUsSuccess',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ContactUsSuccess.vue')
+  },
+  {
     path: '/contactUs',
     name: 'ContactUs',
     // route level code-splitting
---
+++ src/views/ContactUsSuccess.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="contactUsSuccessMainDiv" >
+  <div class="spanSuccess">
+    <p class="tittleSuccess"> YOU'VE CONTACTED US SUCCESSFULYLY!</p>
+    <p class="tittleSuccess"> WE'LL SEND A SUPERHERO TO YOU SOON</p>
+    <p class="tittleSuccess"> THANKS FOR USING NEED2WATCH!</p>
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactUsSuccess',
+};
+</script>
+<style scoped>
+@import '../assets/styles/ContactUsSuccess.css';
+</style>
