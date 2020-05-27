b342732e5887a137b3cb748ba7ebf0692d36d3a0
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-05-27 16:28:17 +0200

    Formatted file

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import router from './router'
-import Vuesax from 'vuesax'
+import router from './router';
+import Vuesax from 'vuesax';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import 'vuesax/dist/vuesax.css'
-import store from "./store";
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import 'vuesax/dist/vuesax.css';
+import store from './store';
 
 Vue.use(Vuesax);
+
 Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
