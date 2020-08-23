cb1bb842e2f6d7b1fbcfe4f89fa12a63ee5f8941
AlexTheMagnus <i72lulem@uco.es>
2020-08-23 17:27:47 +0200

    Added N2wEpisodeInfo component

---
+++ src/components/tvSeriesView/N2wEpisodeInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p class="font-weight-bold">{{this.title}}</p>
+    <p>Season {{this.season}}, Episode {{this.episode}}</p>
+    <p>{{this.description}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'N2wEpisodeInfo',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    season: {
+      type: Number,
+      required: true,
+    },
+    episode: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  data: () => ({}),
+  methods: {},
+};
+</script>
+<style scoped>
+</style>
