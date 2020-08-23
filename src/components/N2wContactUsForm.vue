ffed4404c026c6cb77ad94398eda827a93ac95da
rexuswolf <francisjmp99@gmail.com>
2020-08-23 17:17:02 +0200

    Refactoring forms. Removing CSS

--- src/components/N2wContactUsForm.vue
+++ src/components/N2wContactUsForm.vue
@@ -1,65 +1,45 @@
 <template>
-  <div class="contactUsForm">
-    <p class="title">GET IN TOUCH</p>
-
-    <form class @submit.prevent="onSubmit">
-      <p>
-        <input
-          class="textInputs"
-          type="text"
-          id="name"
-          v-model="name"
-          placeholder="Name..."
-          required
-        />
-      </p>
-      <p>
-        <input
-          class="textInputs"
-          type="text"
-          id="email"
-          v-model="email"
-          placeholder="Email..."
-          required
-        />
-      </p>
-      <p>
-        <input
-          class="textInputs"
-          type="text"
-          id="regarding"
-          v-model="regarding"
-          placeholder="Regarding..."
-          required
-        />
-      </p>
-      <p>
-        <textarea
-          class="textInputs"
-          type="text"
-          id="message"
-          v-model="message"
-          placeholder="Message..."
-          required
-        ></textarea>
-      </p>
-      <p class="terms-text">
-        <input type="checkbox" id="terms" v-model="terms" required />
-        I have read and accept the
-        <n2w-privacy-policy></n2w-privacy-policy>.
-      </p>
-      <p>
-        <input v-on:click="contactSuccess" type="submit" value="SEND IT!" />
-      </p>
-    </form>
-  </div>
+  <v-form @submit.prevent="onSubmit">
+    <v-text-field label="First Name" v-model="name"></v-text-field>
+    <v-text-field label="Last Name" v-model="email"></v-text-field>
+    <v-text-field label="Username" v-model="regarding"></v-text-field>
+    <v-text-area label="City" v-model="message"></v-text-area>
+    <v-btn class="mr-2 primary secondary--text" type="submit">SEND IT!</v-btn>
+    <v-checkbox
+      v-model="checkbox"
+      v-on:keyup.enter="submitForm"
+      :error-messages="checkboxErrors"
+      label="I have read and accept the terms and conditions."
+      required
+      @change="$v.checkbox.$touch()"
+      @blur="$v.checkbox.$touch()"
+    ></v-checkbox>Read the
+    <n2w-privacy-policy />
+  </v-form>
 </template>
 <script>
+import { validationMixin } from 'vuelidate';
+import { required, maxLength, email } from 'vuelidate/lib/validators';
 import N2wPrivacyPolicy from '../components/N2wPrivacyPolicy.vue';
 export default {
   name: 'N2wContactUsForm',
   components: {
     N2wPrivacyPolicy,
+  },
+  mixins: [validationMixin],
+  validations: {
+    firstName: { required, maxLength: maxLength(16) },
+    lastName: { required, maxLength: maxLength(16) },
+    email: { required, email },
+    username: { required, maxLength: maxLength(16) },
+    password: { required, maxLength: maxLength(16) },
+    city: { required, name },
+    select: { required },
+    checkbox: {
+      checked(val) {
+        return val;
+      },
+    },
   },
   data() {
     return {
@@ -67,8 +47,67 @@ export default {
       email: '',
       regarding: '',
       message: '',
-      terms: false,
+      checkbox: false,
     };
+  },
+  computed: {
+    checkboxErrors() {
+      const errors = [];
+      if (!this.$v.checkbox.$dirty) return errors;
+      !this.$v.checkbox.checked && errors.push('You must agree to continue!');
+      return errors;
+    },
+    selectErrors() {
+      const errors = [];
+      if (!this.$v.select.$dirty) return errors;
+      !this.$v.select.required && errors.push('Item is required');
+      return errors;
+    },
+    firstNameErrors() {
+      const errors = [];
+      if (!this.$v.firstName.$dirty) return errors;
+      !this.$v.firstName.maxLength &&
+        errors.push('First Name must be at most 16 characters long');
+      !this.$v.firstName.required && errors.push('First Name is required.');
+      return errors;
+    },
+    lastNameErrors() {
+      const errors = [];
+      if (!this.$v.lastName.$dirty) return errors;
+      !this.$v.lastName.maxLength &&
+        errors.push('Last Name must be at most 16 characters long');
+      !this.$v.lastName.required && errors.push('Last Name is required.');
+      return errors;
+    },
+    usernameErrors() {
+      const errors = [];
+      if (!this.$v.username.$dirty) return errors;
+      !this.$v.username.maxLength &&
+        errors.push('Username must be at most 16 characters long');
+      !this.$v.username.required && errors.push('Username is required.');
+      return errors;
+    },
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.password.$dirty) return errors;
+      !this.$v.password.maxLength &&
+        errors.push('Password must be at most 16 characters long');
+      !this.$v.password.required && errors.push('Password is required.');
+      return errors;
+    },
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.email && errors.push('Must be valid e-mail');
+      !this.$v.email.required && errors.push('E-mail is required');
+      return errors;
+    },
+    cityErrors() {
+      const errors = [];
+      if (!this.$v.city.$dirty) return errors;
+      !this.$v.city.required && errors.push('City is required');
+      return errors;
+    },
   },
   methods: {
     onSubmit() {
@@ -87,6 +126,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-@import '../assets/styles/N2wContactUsForm.css';
-</style>
--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,29 +1,20 @@
 <template>
-  <div class="contactUsMainDiv d-flex justify-center align-center" >
-
-    <div class="formDiv">
-      <N2wContactUsForm></N2wContactusForm>
-    </div>
-
-    <div class="hidden-sm-and-down socialDiv">
-      <v-list-item class="socialNetwork">
-        <v-avatar size="100">
-          <v-img src="../assets/img/facebook.png"></v-img>
-        </v-avatar>
-
-        <v-list-item-title class="socialNetworkText">NEED2WATCH</v-list-item-title>
-      </v-list-item>
-
-      <v-list-item class="socialNetwork">
-        <v-avatar size="100">
-          <v-img src="../assets/img/twitter.png"></v-img>
-        </v-avatar>
-
-        <v-list-item-title class="socialNetworkText">@NEED2WATCH</v-list-item-title>
-      </v-list-item>
-    </div>
-
-  </div>
+  <v-container fill-height fluid>
+    <v-row align="center">
+      <v-col md="6" class="d-flex flex-column align-center">
+        <p class="text-h3">GET IN TOUCH!</p>
+        <n2w-contact-us-form />
+      </v-col>
+      <v-col md="6" class="d-flex flex-column">
+        <v-btn height="100" class="text-h2" text>
+          <v-icon size="80" class="mr-8" left>mdi-facebook</v-icon>Need2Watch
+        </v-btn>
+        <v-btn height="100" text class="text-h2">
+          <v-icon size="80" class="mr-8" left>mdi-twitter</v-icon>Need2Watch
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -36,5 +27,11 @@ export default {
 };
 </script>
 <style scoped>
-@import '../assets/styles/ContactUs.css';
+.container {
+  background-image: url(https://i.imgur.com/pmyWD6B.jpg);
+  background-color: rgba(27, 38, 54, 0.5);
+  background-blend-mode: color-burn;
+  background-position: center;
+  background-size: cover;
+}
 </style>
