4a505e0be3f518630aca11038bed9cbe47304395
rexuswolf <francisjmp99@gmail.com>
2020-08-24 18:37:43 +0200

    Good starting point

--- src/components/moviesView/N2wCarousel.vue
+++ src/components/moviesView/N2wCarousel.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="card-carousel-wrapper n2wgray">
-    <div class="card-carousel--nav__left" @click="moveCarousel(-1)" :disabled="atHeadOfList"></div>
-    <div class="card-carousel n2wgray">
-      <div class="card-carousel--overflow-container">
+  <v-row align="center" justify="center">
+    <v-col class="text-center" cols="1">
+      <v-icon size="60" @click="moveCarousel(-1)" :disabled="atHeadOfList">mdi-chevron-left</v-icon>
+    </v-col>
+    <v-col cols="10">
+      <v-row align="center" class="overflow-hidden">
         <div
-          class="card-carousel-cards"
-          :style="{ transform: 'translateX' + '(' + currentOffset + 'px' + ')' }"
+          class="d-flex card-carousel-cards"
+          :style="{ transform: 'translateX' + '(' + currentOffset + 'px' + ')', }"
         >
-          <div class="card-wrapper" :key="item.title" v-for="item in items">
+          <div class="my-5 mx-10" :key="item.title" v-for="item in items">
             <n2w-cinema-card
               v-bind:id="item.movie_id"
               v-bind:name="item.title"
@@ -17,21 +19,16 @@
             ></n2w-cinema-card>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="card-carousel--nav__right" @click="moveCarousel(1)" :disabled="atEndOfList"></div>
-  </div>
+      </v-row>
+    </v-col>
+    <v-col class="text-center" cols="1">
+      <v-icon size="60" @click="moveCarousel(1)" :disabled="atEndOfList">mdi-chevron-right</v-icon>
+    </v-col>
+  </v-row>
 </template>
 <script>
 import N2wCinemaCard from './N2wCinemaCard.vue';
 export default {
-  created() {
-    this.windowResize();
-    window.addEventListener('resize', this.resizeEventHandler);
-  },
-  destroyed() {
-    window.removeEventListener('resize', this.resizeEventHandler);
-  },
   name: 'N2wCarousel',
   components: {
     N2wCinemaCard,
@@ -62,53 +59,18 @@ export default {
     },
   },
   methods: {
-    showModal() {
-      this.isModalVisible = true;
-    },
-    closeModal() {
-      this.isModalVisible = false;
-    },
     moveCarousel(direction) {
-      // Find a more elegant way to express the :style. consider using props to make it truly generic
       if (direction === 1 && !this.atEndOfList) {
         this.currentOffset -= this.paginationFactor;
       } else if (direction === -1 && !this.atHeadOfList) {
         this.currentOffset += this.paginationFactor;
       }
     },
-    resizeEventHandler() {
-      this.windowResize();
-    },
-    windowResize() {
-      var preSize = this.windowSize;
-
-      if (window.matchMedia('(max-width: 554px)').matches) {
-        this.windowSize = 1;
-        return 0;
-      }
-      if (window.matchMedia('(max-width: 816px)').matches) {
-        this.windowSize = 2;
-        return 0;
-      }
-      if (window.matchMedia('(max-width: 1078px)').matches) {
-        this.windowSize = 3;
-        return 0;
-      }
-      if (window.matchMedia('(max-width: 1340px)').matches) {
-        this.windowSize = 4;
-        return 0;
-      }
-      this.windowSize = 5;
-
-      if (preSize < this.windowSize) {
-        for (var i = 0; i < this.windowSize - preSize; i++) {
-          this.moveCarousel(-1);
-        }
-      }
-    },
   },
 };
 </script>
 <style scoped>
-@import '../../assets/styles/N2wCarousel.css';
+.card-carousel-cards {
+  transition: transform 150ms ease-out;
+}
 </style>
