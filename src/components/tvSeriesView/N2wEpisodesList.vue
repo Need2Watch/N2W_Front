9811817e00581e44dbd0ad738bdcec5b2f5df0ab
AlexTheMagnus <i72lulem@uco.es>
2020-08-25 23:54:38 +0200

    Added TvSeries component's events

--- src/components/tvSeriesView/N2wEpisodesList.vue
+++ src/components/tvSeriesView/N2wEpisodesList.vue
@@ -6,16 +6,18 @@
       </v-col>
       <v-spacer />
       <v-col>
-        <v-btn text>
-          <v-icon left>mdi-eye</v-icon>Mark season as seen
-        </v-btn>
+        <v-btn text> <v-icon left>mdi-eye</v-icon>Mark season as seen </v-btn>
       </v-col>
     </v-row>
     <v-col cols="12">
       <v-list color="transparent">
-        <v-list-item v-for="(item, index) in episodes" :key="item.title" @click="{}">
+        <v-list-item
+          v-for="(item, index) in episodes"
+          :key="item.title"
+          @click="updateSelectedEpisode(index)"
+        >
           <v-list-item-content>
-            <v-list-item-title>{{index}}. {{item.title}}</v-list-item-title>
+            <v-list-item-title>{{ index }}. {{ item.title }}</v-list-item-title>
           </v-list-item-content>
 
           <v-list-item-action>
@@ -35,40 +37,17 @@
 <script>
 export default {
   name: 'N2wEpisodesList',
-  data: () => ({
-    episodes: [
-      {
-        title: 'Rose',
-        seen: true,
-      },
-      {
-        title: 'The End of the world',
-        seen: true,
-      },
-      {
-        title: 'The Unquiet Dead',
-        seen: true,
-      },
-      {
-        title: 'Aliens on London',
-        seen: true,
-      },
-      {
-        title: 'World War Three',
-        seen: false,
-      },
-      {
-        title: 'Dalek',
-        seen: false,
-      },
-      {
-        title: 'The Long Game',
-        seen: false,
-      },
-    ],
-  }),
-  methods: {},
+  props: {
+    episodes: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    updateSelectedEpisode(episode) {
+      this.$emit('clickedEpisode', episode);
+    },
+  },
 };
 </script>
-<style scoped>
-</style>
+<style scoped></style>
