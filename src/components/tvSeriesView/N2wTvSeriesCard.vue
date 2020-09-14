997004beb3cd308fc97d485ac19047d3567f6b0a
rexuswolf <francisjmp99@gmail.com>
2020-09-14 18:32:50 +0200

    Looking good atm

--- src/components/tvSeriesView/N2wTvSeriesCard.vue
+++ src/components/tvSeriesView/N2wTvSeriesCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container data-testid="card" fluid fill-height>
+  <v-card data-testid="card" color="secondary" v-bind="$attrs" outlined>
     <v-row>
-      <v-col cols="5" class="pt-0 pb-0">
+      <v-col md="3" sm="4" cols="12" class="pt-0 pb-0">
         <v-img :src="movie.poster_url">
           <v-row align="start">
             <v-col cols="1" class="ml-4">
@@ -11,27 +11,17 @@
           </v-row>
         </v-img>
       </v-col>
-      <v-col cols="7">
-        <div class="text-h4" v-text="this.movie.title"></div>
-        <div class="text-subtitle-1">2018 - 2 h 23 min - 16+</div>
-        <v-tabs background-color="transparent" fixed-tabs v-model="tab">
-          <v-tab v-for="item in items" :key="item.tab">{{ item.tab }}</v-tab>
-        </v-tabs>
-
-        <v-tabs-items class="transparent" v-model="tab">
-          <v-tab-item v-for="item in items" :key="item.tab">
-            <v-card class="transparent" flat>
-              <v-card-text>{{ item.content }}</v-card-text>
-            </v-card>
-          </v-tab-item>
-        </v-tabs-items>
-        <div v-if="this.loggedUser.userId">
+      <v-col md="9" sm="8" cols="12">
+        <v-card-title class="text-h4" v-text="this.movie.title"></v-card-title>
+        <v-card-subtitle v-text="this.movieGenres"></v-card-subtitle>
+        <v-card-text class="text-h5">{{this.movie.overview}}</v-card-text>
+        <v-card-actions v-if="this.loggedUser.userId" class="movieCardActions">
           <n2w-follow-button />
           <n2w-watch-button />
-        </div>
+        </v-card-actions>
       </v-col>
     </v-row>
-  </v-container>
+  </v-card>
 </template>
 <script>
 import { mapGetters } from 'vuex';
@@ -40,31 +30,15 @@ import N2wFollowButton from '../buttons/N2wFollowButton.vue';
 import N2wWatchButton from '../buttons/N2wWatchButton.vue';
 
 export default {
-  name: 'N2wMovieCard',
-  data() {
-    return {
-      tab: null,
-    };
-  },
+  name: 'N2wTvSeriesCard',
   computed: {
     ...mapGetters({
       loggedUser: 'loggedUser/loggedUser',
       movie: 'currentMovie/currentMovie',
     }),
-    items() {
-      return [
-        { tab: 'Overview', content: this.movie.overview },
-        { tab: 'Trailers & More', content: 'Tab 2 Content' },
-        { tab: 'Reviews', content: 'Tab 3 Content' },
-        { tab: 'Details', content: 'Tab 4 Content' },
-      ];
-    },
     starCount() {
       let starCountArray = parseInt(this.movie.rating / 2);
       return starCountArray;
-    },
-    movie() {
-      return this.$store.getters['currentMovie/currentMovie'];
     },
     ratingIsOdd() {
       if (this.movie.rating % 2 != 0) return true;
@@ -84,3 +58,10 @@ export default {
   },
 };
 </script>
+<style scoped>
+.movieCardActions {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="home fill-height">
     <v-container fill-height fluid>
-      <v-row justify="center">
+      <v-row justify="center" align="center">
         <v-col class="d-flex flex-column align-center" md="8" cols="12">
           <v-img max-width="70%" src="../assets/img/logo-extended.png"></v-img>
-          <p
-            class="d-flex flex-column align-center title font-weight-black n2wwhite--text"
-          >Control your movie night schedule</p>
+          <div class="text-h5 font-weight-black">Control your movie night schedule</div>
         </v-col>
-        <v-col align-self="center" class="d-flex flex-column align-center" md="6" cols="12">
-          <p>Join Need2Watch now. Your favourite platform for managing your TV Series and films. In N2W you'll receive our best recomendations based on your taste. Start managing your series scheduling their episodes, follow your friends and discover new content. Everything is easier with Need2Watch, we'll make your life easier so...come on and join us! It's free!</p>
+        <v-col class="d-flex flex-column align-center" md="6" cols="12">
+          <p>Join Need2Watch now. Your favourite platform for managing your TV series and films. In N2W you'll receive our best recomendations based on your taste. Start managing your series scheduling their episodes, follow your friends and discover new content. Everything is easier with Need2Watch, we'll make your life easier so...come on and join us! It's free!</p>
           <v-btn
+            width="50%"
             color="primary"
-            class="n2wblue--text font-weight-black"
+            class="secondary--text font-weight-black"
             href="https://github.com/Need2Watch"
             target="_blank"
           >JOIN NEED2WATCH FOR FREE</v-btn>
         </v-col>
-        <v-col align-self="center" cols="6">
+        <v-col align-self="center" md="6" cols="12">
           <v-card>
-            <v-carousel cycle hide-delimiter-background show-arrows-on-hover>
+            <v-carousel
+              height="350px"
+              hide-delimiters
+              cycle
+              hide-delimiter-background
+              show-arrows-on-hover
+            >
               <v-carousel-item
                 v-for="(slide, i) in slides"
                 :key="i"
@@ -31,7 +36,7 @@
         </v-col>
       </v-row>
     </v-container>
-    <v-footer app color="primary" class="n2wblue--text">
+    <v-footer app color="primary" class="secondary--text">
       <v-spacer></v-spacer>
       <div>Copyright &copy; {{ new Date().getFullYear() }} Need2Watch, inc. All rights reserved.</div>
     </v-footer>
