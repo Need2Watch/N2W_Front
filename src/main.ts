494248869493398f491a6563d078489dca71ec1d
rexuswolf <francisjmp99@gmail.com>
2020-09-20 17:59:02 +0200

    Changed NavBar folder

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from './App/App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
 import axios from 'axios';
