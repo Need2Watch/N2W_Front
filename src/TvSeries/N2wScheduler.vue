0ec88449b0baa34dfd85b6541f081281a0cc379a
AlexTheMagnus <i72lulem@uco.es>
2020-09-29 18:40:56 +0200

    Install vue-numeric-input and n2w-schedule-button wip

--- src/TvSeries/N2wScheduler.vue
+++ src/TvSeries/N2wScheduler.vue
@@ -9,15 +9,25 @@
       How many episodes would you like to see per day?
     </v-card-title>
 
-    <v-container class="center">
+    <v-container>
       <v-row>
-        <v-col>
-          <v-btn-toggle v-model="toggle_none">
-            <v-btn flat v-for="item in 7" v-bind:key="item">
-              <v-icon>mdi-plus-circle-outline</v-icon>
-            </v-btn>
-          </v-btn-toggle>
+        <v-spacer />
+        <v-col
+          class="d-flex flex-column align-center"
+          cols="1"
+          v-for="index in 7"
+          v-bind:key="index"
+        >
+          <p>{{ weekDays[index - 1] }}</p>
+          <vue-numeric-input
+            v-model="episodesPerDay[index - 1]"
+            :min="0"
+            size="40px"
+            align="center"
+            controls-type="updown"
+          ></vue-numeric-input>
         </v-col>
+        <v-spacer />
       </v-row>
     </v-container>
 
@@ -35,7 +45,52 @@
 </template>
 
 <script>
+import VueNumericInput from 'vue-numeric-input';
+
 export default {
   name: 'N2wScheduler',
+  data: () => ({
+    weekDays: ['M', 'T', 'W', 'R', 'F', 'S', 'U'],
+    episodesPerDay: [0, 0, 0, 0, 0, 0, 0],
+  }),
+  computed: {
+    episodesPerWeek: function () {
+      return this.episodesPerDay.reduce((a, b) => a + b, 0);
+    },
+  },
+  components: {
+    VueNumericInput,
+  },
 };
 </script>
+
+<style>
+.vue-numeric-input.updown {
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+  background: #ffffff !important;
+}
+.vue-numeric-input.updown .numeric-input {
+  padding-right: 5px !important;
+  padding-left: 5px !important;
+}
+.vue-numeric-input.updown .btn {
+  background: gold !important;
+}
+.vue-numeric-input.updown .btn-increment {
+  height: 1.5rem;
+  width: 100%;
+  right: 0 !important;
+  left: 0 !important;
+  top: 0 !important;
+  bottom: auto !important;
+}
+.vue-numeric-input.updown .btn-decrement {
+  height: 1.5rem;
+  width: 100%;
+  left: 0 !important;
+  right: 0 !important;
+  top: auto !important;
+  bottom: 0 !important;
+}
+</style>
